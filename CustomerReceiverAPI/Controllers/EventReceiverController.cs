469ebc7dd10fa74cb415125f55bd3671987af2da
Garima <garima.gupta6688@gmail.com>
2026-07-01 11:26:55 +0530

    firstdeploy

---
+++ CustomerReceiverAPI/Controllers/EventReceiverController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerReceiverAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventReceiverController : ControllerBase
+    {
+        public IActionResult Receive([FromBody] object data)
+        {
+           Console.WriteLine(data.ToString());
+            return Ok();
+        }
+    }
+}
